e64ee428efd74e2248ae8dd9a623123cedac29b4
Jedidiah Abera <yididi@Jedidiah>
2024-10-04 00:23:56 +0300

    Done creating: 3-currency.js

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,26 @@
+export default class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set name(name) {
+    this._name = name;
+  }
+
+  set code(code) {
+    this._code = code;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this.code})`;
+  }
+}
