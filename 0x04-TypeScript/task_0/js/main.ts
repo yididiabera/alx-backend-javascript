c6da452860b16cf91b0257e3f44d8aae80759c68
Jedidiah Abera <yididiabera87@gmail.com>
2024-10-09 21:38:11 +0300

    Done creating: task_0

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,45 @@
+interface Student {
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: string;
+}
+
+const student1: Student = {
+  firstName: "Abenezer",
+  lastName: "Endalew",
+  age: 21,
+  location: "Addis Ababa",
+}
+const student2: Student = {
+  firstName: "Abebe",
+  lastName: "Fekadu",
+  age: 15,
+  location: "Nairobi",
+}
+const studentsList: Array<Student> = [student1, student2];
+
+const table = document.createElement("table");
+const thead = document.createElement("thead");
+const header = document.createElement("tr");
+
+const headers = Object.keys(studentsList[0]);
+headers.forEach((h) => {
+  const th = document.createElement("th");
+  th.textContent = h.toString();
+  header.appendChild(th);
+});
+thead.appendChild(header);
+table.appendChild(thead);
+const tbody = document.createElement("tbody");
+studentsList.forEach((student) => {
+  const tr = document.createElement("tr");
+  headers.forEach((h) => {
+    const td = document.createElement("td");
+    td.textContent = String(student[h as keyof Student]);
+    tr.appendChild(td);
+  });
+  tbody.appendChild(tr);
+});
+table.appendChild(tbody);
+document.body.appendChild(table);
