115917aea4c6b7797b43422e0f3b2e1a83993f7a
Jedidiah Abera <140195943+yididiabera@users.noreply.github.com>
2024-11-27 01:00:58 +0300

    Done creating: 4-http.js

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+const app = http.createServer((req, res) => {
+  res.end('Hello Holberton School!');
+});
+
+const port = 1245;
+app.listen(port);
+
+module.exports = app;
