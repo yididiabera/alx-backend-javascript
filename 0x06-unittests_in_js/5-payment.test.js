d91efba51695b988341d699645aaa7750f3f3ed1
Jedidiah Abera <140195943+yididiabera@users.noreply.github.com>
2024-11-28 23:37:33 +0300

    Done creating: 5-payment.test.js

---
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -0,0 +1,27 @@
+const sendPaymentRequestToApi = require('./4-payment');
+const sinon = require('sinon');
+const { expect } = require('chai');
+
+describe('Test Payment', () => {
+  let spy;
+
+  beforeEach(() => {
+    spy = sinon.spy(console, 'log');
+  });
+
+  afterEach(() => {
+    spy.restore();
+  });
+
+  it('should log 120 when called with 100 and 20', () => {
+    sendPaymentRequestToApi(100, 20);
+    expect(spy.calledWith('The total is: 120')).to.be.true;
+    expect(spy.calledOnce).to.be.true;
+  });
+
+  it('should log 20 when called with 10 and 10', () => {
+    sendPaymentRequestToApi(10, 10);
+    expect(spy.calledWith('The total is: 20')).to.be.true;
+    expect(spy.calledOnce).to.be.true;
+  });
+})
